080ecf64e0c56c1fcc9704961475e9bff9cb4f15
Mohamed <mohakatalonya10@gmail.com>
2024-08-19 19:50:44 +0200

    refactoring the node to express code

--- app.js
+++ app.js
@@ -1,61 +1,82 @@
-const http = require('http');
+// const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const EventEmitter = require('events');
 
+const express = require('express');
+const app = express();
+
 const eventEmitter = new EventEmitter();
 
-const port = 8080;
+const PORT = process.env.PORT ||  8080;
 
 eventEmitter.on('start', () => {
   console.log('started');
 });
 
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    eventEmitter.emit('start' /*optional arguments */);
-    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-      }
-    );
-  } else if (req.url === '/contact-me') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'contact-me.html'),
-      (err, content) => {
-        if (err) {
-          throw err;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-      }
-    );
-  } else if (req.url === '/about') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'about.html'),
-      (err, content) => {
-        if (err) {
-          throw err;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-      }
-    );
-  } else {
-    fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+app.get('/', (req, res) =>{
+  eventEmitter.emit('start' /*optional arguments */);
+  fs.readFile(
+    path.join(__dirname, 'public', 'index.html'),
+    (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  );
+})
+app.get('/contact-me', (req, res) =>{
+  fs.readFile(
+    path.join(__dirname, 'public', 'contact-me.html'),
+    (err, content) => {
       if (err) {
         throw err;
       }
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(content);
-    });
-  }
-});
+    }
+  );
+})
+app.get('/about', (req, res) =>{
+  fs.readFile(
+    path.join(__dirname, 'public', 'about.html'),
+    (err, content) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  );
+})
+app.get('*', (req, res) =>{
+  fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+    if (err) {
+      throw err;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+})
 
-server.listen(process.env.PORT || port, () =>
-  console.log('Server is up and running on localhost:' + port)
+app.listen(PORT, () =>
+  console.log('Server is up and running on localhost:' + PORT)
 );
+
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+    
+//   } else if (req.url === '/contact-me') {
+    
+//   } else if (req.url === '/about') {
+   
+//   } else {
+    
+//   }
+// });
+
+// server.listen(process.env.PORT || port, () =>
+//   console.log('Server is up and running on localhost:' + port)
+// );
